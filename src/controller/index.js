04cb620ac8496582743626fefa7ce69187527884
vponugumati@my.ocku.edu <vponugumati@my.ocku.edu>
2024-04-18 20:14:22 +0300

    Create app

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,7 +1,7 @@
 import { activitySerializer } from "../model";
 
 export const getActivity = async () => {
-  const res = await fetch('https://.boredapi.com/api/activity/');
+  const res = await fetch('https://boredapi.com/api/activity/');
   const data = await res.json()
 
   return activitySerializer(data);
